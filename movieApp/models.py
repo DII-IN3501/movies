d2f9fc8500761f727856cf0effe2c2296231c32e
Benjamín Hurtado Torrealba <benjamin.hurtado@ing.uchile.cl>
2021-05-25 08:02:50 -0400

    Added models

--- movieApp/models.py
+++ movieApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=255)
+    estreno = models.DateField()
+    genero = models.CharField(max_length=255)
+    idioma = models.CharField(max_length=7)
+
+class Subtitulo(models.Model):
+    idioma = models.CharField(max_length=7)
+    contenido = models.FileField(upload_to="uploads/")
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=255)
+    nacionalidad = models.CharField(max_length=255)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Participacion(models.Model):
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    cargo = models.CharField(max_length=255)
